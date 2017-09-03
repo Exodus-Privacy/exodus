6cb53e9367e9f92ac4a0be461942132916a7dbe7
U+039b <git@0x39b.fr>
2017-09-03 19:41:05 +0200

    Improve reports dashboard

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ import json, time
 import subprocess as sp
 import requests
 
+PARTIAL = True
+
 # Programs
 gplaycli = "/usr/local/bin/gplaycli"
 adb = "/home/lambda/Android/Sdk/platform-tools/adb"
@@ -17,7 +19,7 @@ vm_ip = "192.168.1.110"
 vm_snapshot = "Android-setup-geoloc"
 
 # Tcpdump
-tcpdump_duration = 60
+tcpdump_duration = 80
 iface = "wlp4s0"
 dns_ip = "192.168.1.108"
 tcpdump_filter = "\"dst host 192.168.1.110 or src host 192.168.1.110\""
@@ -39,13 +41,13 @@ def os_run(cmd):
 
     
 # Clear all
-os.system("rm -rf %s/*" % apk_folder)
+# os.system("rm -rf %s/*" % apk_folder)
 os.system("rm -rf %s/*" % extracted_folder)
 os.system("rm -rf %s/*" % decoded_folder)
 os.system("rm -rf %s/*" % net_folder)
 
 # Download APK
-os.system("/usr/local/bin/gplaycli -F %s -f %s" % (apk_list_file, apk_folder))
+# os.system("/usr/local/bin/gplaycli -F %s -f %s" % (apk_list_file, apk_folder))
 
 # For each applications
 apk_names = [fn for fn in os.listdir(apk_folder)
@@ -68,76 +70,84 @@ for apk_name in apk_names:
     print("Lookup in %s" % apk_name)
     app_data = app.get_data(apk_name, apk_path, decoded_apk_path)
     app_data['report']['trackers'] = trackers.find_all(extracted_apk_path)
-
-    handle = app_data['application']['handle']
-    pcap_output = '%s/%s.pcap' % (net_folder, apk_name)
-
-    print("Starting VirtualBox VM")
-    cmd = 'vboxmanage controlvm "%s" poweroff' % vm_name
-    if os_run(cmd) != 0:
-        print("** Aborting analysis of %s - Unable to stop VM" % apk_name)
-    #     break
-    time.sleep(5)
-    cmd = 'vboxmanage snapshot "%s" restore %s' % (vm_name, vm_snapshot)
-    if os_run(cmd) != 0:
-        print("** Aborting analysis of %s - Unable to restore snapshot" % apk_name)
-        break
-    time.sleep(5)
-    cmd = 'vboxmanage startvm "%s"' % vm_name
-    if os_run(cmd) != 0:
-        print("** Aborting analysis of %s - Unable to start VM" % apk_name)
-        break
-    time.sleep(10)
-
-    print("Connecting ADB to Android")
-    cmd = '%s connect %s' % (adb, vm_ip)
-    print(cmd)
-    if os_run(cmd) != 0:
-        break 
-    time.sleep(5)
-    if os_run(cmd) != 0:
-        break 
-    time.sleep(5)
-    print("Installing %s" % apk_name)
-    cmd = '%s install %s' % (adb, apk_path)
-    print(cmd)
-    if os_run(cmd) != 0:
-        print("** Aborting analysis of %s - Unable to install app" % apk_name)  
-        break  
-    cmd = '%s shell pm grant %s android.permission.ACCESS_COARSE_LOCATION' % (adb, handle)
-    print(cmd)
-    if os_run(cmd) != 0:
-        print("** Ooooops Unable to set permission")  
-    
-    print("Starting network sniffing")
-    cmd = "/usr/bin/sudo /usr/sbin/tcpdump -G %s -W 1 -w %s -i %s %s" % (tcpdump_duration, pcap_output, iface, tcpdump_filter)
-    print(cmd)
-    p = sp.Popen(cmd, stdout=sp.PIPE, shell=True)
-    time.sleep(5)
-
-    print("Starting %s application" % apk_name)
-    cmd = '%s shell monkey -p %s 1' % (adb, handle)
-    if os_run(cmd) != 0:
-        print("** Aborting analysis of %s - Unable to start application" % apk_name)  
-        break  
-    cmd = '%s disconnect %s' % (adb, vm_ip)
-    print(cmd)
-    if os_run(cmd) != 0:
-        break 
-
-    time.sleep(tcpdump_duration)
-
-    print("Stopping VirtualBox VM")
-    cmd = 'vboxmanage controlvm "%s" poweroff' % vm_name
-    if os_run(cmd) != 0:
-        print("** Aborting analysis of %s - Unable to stop VM" % apk_name)
-        break
-
-    print("Analysing network activity of %s" % apk_name)
     app_data['report']['network'] = {}
-    app_data['report']['network']['dns'] = pcap.get_dns(pcap_output)
     app_data['report']['network']['http'] = {}
-    app_data['report']['network']['http']['post'] = pcap.get_http_post(pcap_output)
+
+    if PARTIAL:
+        app_data['report']['partial'] = True
+
+    if not PARTIAL:
+        handle = app_data['application']['handle']
+        pcap_output = '%s/%s.pcap' % (net_folder, apk_name)
+
+        print("Starting VirtualBox VM")
+        cmd = 'vboxmanage controlvm "%s" poweroff' % vm_name
+        if os_run(cmd) != 0:
+            print("** Oooops Unable to stop VM")
+        #     break
+        time.sleep(5)
+        cmd = 'vboxmanage snapshot "%s" restore %s' % (vm_name, vm_snapshot)
+        if os_run(cmd) != 0:
+            print("** Aborting analysis of %s - Unable to restore snapshot" % apk_name)
+            continue
+        time.sleep(5)
+        cmd = 'vboxmanage startvm "%s"' % vm_name
+        if os_run(cmd) != 0:
+            print("** Aborting analysis of %s - Unable to start VM" % apk_name)
+            continue
+        time.sleep(10)
+
+        print("Connecting ADB to Android")
+        cmd = '%s connect %s' % (adb, vm_ip)
+        
+        if os_run(cmd) != 0:
+            continue 
+        time.sleep(5)
+        if os_run(cmd) != 0:
+            continue 
+        time.sleep(5)
+        print("Installing %s" % apk_name)
+        cmd = '%s install %s' % (adb, apk_path)
+        
+        if os_run(cmd) != 0:
+            print("** Aborting analysis of %s - Unable to install app" % apk_name)  
+            continue  
+        
+        print("Starting network sniffing")
+        cmd = "/usr/bin/sudo /usr/sbin/tcpdump -G %s -W 1 -w %s -i %s %s" % (tcpdump_duration, pcap_output, iface, tcpdump_filter)
+        
+        p_tcpdump = sp.Popen(cmd, stdout=sp.PIPE, shell=True)
+        time.sleep(5)
+
+        print("Starting %s application" % apk_name)
+        cmd = '%s shell monkey -p %s 25' % (adb, handle)
+        if os_run(cmd) != 0:
+            print("** Aborting analysis of %s - Unable to start application" % apk_name)  
+            continue  
+        cmd = '%s shell pm grant %s android.permission.ACCESS_COARSE_LOCATION' % (adb, handle)
+        
+        if os_run(cmd) != 0:
+            print("** Ooooops Unable to set permission")  
+        cmd = '%s disconnect %s' % (adb, vm_ip)
+        
+        if os_run(cmd) != 0:
+            continue 
+
+        time.sleep(tcpdump_duration)
+
+        print("Stopping VirtualBox VM")
+        cmd = 'vboxmanage controlvm "%s" poweroff' % vm_name
+        if os_run(cmd) != 0:
+            print("** Aborting analysis of %s - Unable to stop VM" % apk_name)
+            continue
+        time.sleep(30)
+
+        # Wait tcpdump to write the pcap file
+        p_tcpdump.communicate()[0]
+
+        print("Analysing network activity of %s" % apk_name)
+        app_data['report']['network']['dns'] = pcap.get_dns(pcap_output)
+        app_data['report']['network']['http']['post'] = pcap.get_http_post(pcap_output)
 
     print("Exporting report of %s" % apk_name)
     print json.dumps(app_data, sort_keys=True, indent=4)
--- server/run.py
+++ server/run.py
@@ -1,8 +1,11 @@
 import sys, os
 import json, time
+import pymongo
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 from flask import Flask
 from flask import request
+from flask import render_template
 from flask_script import Manager
 from bson.json_util import dumps, RELAXED_JSON_OPTIONS
 
@@ -35,6 +38,48 @@ def get_reports():
     )
     return response
 
-    
+@app.route('/app/<name>', methods=['GET'])
+def app_report(name='fr.meteo'):
+    client = MongoClient('localhost', 27017)
+    db = client[exodus]
+    reports = db[exodus_reports]
+    report = reports.find({'application.handle':name}).sort('report.date',pymongo.DESCENDING).limit(1)[0]
+    return render_template('app_report.html', report=report)
+
+@app.route('/appid/<idt>', methods=['GET'])
+def app_id(idt='0'):
+    client = MongoClient('localhost', 27017)
+    db = client[exodus]
+    reports = db[exodus_reports]
+    report = reports.find({"_id": ObjectId(idt)}).limit(1)[0]
+    return render_template('app_report.html', report=report)
+
+
+@app.route('/allreports/<partial>', methods=['GET'])
+def all_reports(partial=0):
+    client = MongoClient('localhost', 27017)
+    db = client[exodus]
+    reports = db[exodus_reports]
+    r = []
+    apps = reports.distinct("application.handle")
+    apps.sort()
+    trackers = reports.distinct("report.trackers")
+    trackers.sort()
+    for a in apps: 
+        if partial == "0":
+            res = reports.find({"$and": [{'application.handle':a}, {'report.partial':{ "$exists": False}}]}).sort('report.date',pymongo.DESCENDING).limit(1)
+            if res.count() > 0:
+                r.append(res[0])
+            # r.append(reports.find({'application.handle':a}).sort('report.date',pymongo.DESCENDING).limit(1)[0])
+        else:
+            res = reports.find({"$and": [{'application.handle':a}, {'report.partial':{ "$exists": True}}]}).sort('report.date',pymongo.DESCENDING).limit(1)
+            if res.count() > 0:
+                r.append(res[0])
+    return render_template('all_reports.html', reports=r, trackers=trackers)
+
+@app.route('/allreports', methods=['GET'])
+def non_partial():
+    return all_reports("0")
+
 if __name__ == "__main__":
     manager.run()
