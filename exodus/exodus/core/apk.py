7299f229f2aac11c08c4b8baa8c77c389bf58fcd
U+039b <git@0x39b.fr>
2017-10-31 22:14:21 +0100

    Fix #9

--- exodus/exodus/core/apk.py
+++ exodus/exodus/core/apk.py
@@ -27,7 +27,7 @@ def grep(folder, pattern):
 def find_and_save_app_icon(self, analysis):
     cmd = "aapt d --values badging %s | grep application-icon | tail -n1 | cut -d \"'\" -f2" % analysis.apk_path
     process = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT)
-    icon = str(process.communicate()[0]).replace('\n', '')
+    icon = process.communicate()[0].decode(encoding='UTF-8').replace('\n', '')
     print(cmd)
     print(icon)
     exitCode = process.returncode
