f8308f79af2bd4cf93e1a8def86b603913ebfcb0
U+039b <git@0x39b.fr>
2017-11-01 17:19:57 +0100

    Fix #14 - Add statistics view

--- exodus/reports/urls.py
+++ exodus/reports/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<report_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^apps/$', views.get_all_apps, name='get_all_apps'),
+    url(r'^stats/$', views.get_stats, name='get_stats'),
     url(r'^search/(?P<handle>.+)$', views.search_by_handle, name='search_by_handle'),
     # url(r'^refresh_dns/$', views.refreshdns, name='refreshdns'),
 ]
