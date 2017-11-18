5c060b3e4343b7230bca231126dbf7bdc5113f76
U+039b <git@0x39b.fr>
2017-11-18 20:05:56 +0100

    Refresh analysis for a given report

--- exodus/reports/management/commands/refreshstaticanalysis.py
+++ exodus/reports/management/commands/refreshstaticanalysis.py
@@ -36,38 +36,37 @@ class Command(BaseCommand):
         for report in reports:
             self.stdout.write(
                 self.style.SUCCESS('Start updating report "%s"' % report.id))
-            tmpdir = tempfile.mkdtemp()
-            decoded_dir = os.path.join(tmpdir, 'decoded')
-            icon_name = '%s_%s.png' % (report.bucket, report.application.handle)
-            apk_name = report.apk_file
-            apk_tmp = os.path.join(tmpdir, apk_name)
+            with tempfile.mkdtemp() as tmpdir:
+                decoded_dir = os.path.join(tmpdir, 'decoded')
+                icon_name = '%s_%s.png' % (report.bucket, report.application.handle)
+                apk_name = report.apk_file
+                apk_tmp = os.path.join(tmpdir, apk_name)
 
-            # Download APK from storage
-            minio_client = Minio(settings.MINIO_URL,
-                         access_key=settings.MINIO_ACCESS_KEY,
-                         secret_key=settings.MINIO_SECRET_KEY,
-                         secure=settings.MINIO_SECURE)
-            try:
-                data = minio_client.get_object(settings.MINIO_BUCKET, apk_name)
-                with open(apk_tmp, 'wb') as file_data:
-                    for d in data.stream(32 * 1024):
-                        file_data.write(d)
-            except ResponseError as err:
-                print(err)
-            # Decode APK
-            if decodeAPK(apk_tmp, decoded_dir):
-                # Refresh trackers
-                trackers = findTrackers(decoded_dir)
-                if len(trackers) > len(report.found_trackers.all()):
-                    report.found_trackers = trackers
-                    report.save()
-                    self.stdout.write(self.style.SUCCESS('Successfully update trackers list of "%s"' % report.application.handle))
-                # Refresh icon
-                icon_path = getIcon(icon_name, report.application.handle)
-                if icon_path != '':
-                    report.application.icon_path = icon_path
-                    report.application.save()
-                    self.stdout.write(self.style.SUCCESS('Successfully update icon of "%s"' % report.application.handle))
+                # Download APK from storage
+                minio_client = Minio(settings.MINIO_URL,
+                             access_key=settings.MINIO_ACCESS_KEY,
+                             secret_key=settings.MINIO_SECRET_KEY,
+                             secure=settings.MINIO_SECURE)
+                try:
+                    data = minio_client.get_object(settings.MINIO_BUCKET, apk_name)
+                    with open(apk_tmp, 'wb') as file_data:
+                        for d in data.stream(32 * 1024):
+                            file_data.write(d)
+                except ResponseError as err:
+                    print(err)
+                # Decode APK
+                if decodeAPK(apk_tmp, decoded_dir):
+                    # Refresh trackers
+                    trackers = findTrackers(decoded_dir)
+                    if len(trackers) > len(report.found_trackers.all()):
+                        report.found_trackers = trackers
+                        report.save()
+                        self.stdout.write(self.style.SUCCESS('Successfully update trackers list of "%s"' % report.application.handle))
+                    # Refresh icon
+                    icon_path = getIcon(icon_name, report.application.handle)
+                    if icon_path != '':
+                        report.application.icon_path = icon_path
+                        report.application.save()
+                        self.stdout.write(self.style.SUCCESS('Successfully update icon of "%s"' % report.application.handle))
 
-            shutil.rmtree(tmpdir, ignore_errors=True)
 
