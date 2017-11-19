dd93f1c96a23b472ea951bef22f2990fcd0099cd
U+039b <git@0x39b.fr>
2017-11-19 14:19:39 +0100

    Vet

--- electra/utils.py
+++
@@ -1,6 +0,0 @@
-import subprocess as sp
-
-def os_run(cmd):
-    process = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT)
-    output = process.communicate()[0]
-    return process.returncode 
