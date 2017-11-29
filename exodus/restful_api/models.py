7bf2924e269740bca83e3e4f9d4ee736b734e3a5
U+039b <git@0x39b.fr>
2017-11-29 16:09:19 +0100

    Add REST API for android application

--- exodus/restful_api/models.py
+++ exodus/restful_api/models.py
@@ -22,3 +22,26 @@ class ReportInfos(models.Model):
     pcap_upload_link = models.CharField(max_length=500)
     flow_upload_link = models.CharField(max_length=500)
 
+
+class LightReport(models.Model):
+    creation_date = models.DateTimeField()
+    report_id = models.IntegerField()
+    web_report_url = models.CharField(max_length=500)
+    api_report_url = models.CharField(max_length=500)
+    application_handle = models.CharField(max_length=500)
+    application_version = models.CharField(max_length=500)
+    application_version_code = models.CharField(max_length=500)
+    trackers_count = models.IntegerField()
+    permission_count = models.IntegerField()
+
+    def __init__(self, report):
+        self.creation_date = report.creation_date
+        self.report_id = report.id
+        # FixMe get rid of constant string
+        self.web_report_url = 'https://reports.exodus-privacy.eu.org/reports/%s/' % report.id
+        self.api_report_url = 'https://reports.exodus-privacy.eu.org/api/report/%s/details' % report.id
+        self.application_handle = report.application.handle
+        self.application_version = report.application.version
+        self.application_version_code = report.application.version_code
+        self.trackers_count = len(report.found_trackers.all())
+        self.permission_count = len(report.application.permissions.all())
