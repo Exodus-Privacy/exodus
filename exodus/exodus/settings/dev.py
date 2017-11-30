1e61aa07f4c4f331cac453ec95d446e4818f39bc
daimrod <daimrod@exodus-privacy.eu.org>
2017-11-30 12:13:56 +0100

    Fix the static/ configuration in the development environment

--- exodus/exodus/settings/dev.py
+++ exodus/exodus/settings/dev.py
@@ -16,8 +16,10 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '..','static/')
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '..', 'static'), )
 
 CELERY_BROKER_URL = 'pyamqp://guest@localhost//'
 BROKER_URL = CELERY_BROKER_URL
