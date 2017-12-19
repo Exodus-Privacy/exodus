bf7acff55cbbbd3e3820a0fa9ba701ee2397da19
U+039b <git@0x39b.fr>
2017-12-19 12:53:16 +0100

    DOES NOT Fix worker memory leak :'( Missing file

---
+++ exodus/exodus/core/storage.py
@@ -0,0 +1,64 @@
+from IPython.core.magics import logging
+from django.conf import settings
+from minio import Minio
+from minio.error import (ResponseError, BucketAlreadyOwnedByYou, BucketAlreadyExists)
+
+
+class RemoteStorageHelper():
+    def __init__(self, prefix):
+        self.prefix = prefix
+        self.minio_client = Minio(settings.MINIO_URL,
+                                  access_key = settings.MINIO_ACCESS_KEY,
+                                  secret_key = settings.MINIO_SECRET_KEY,
+                                  secure = settings.MINIO_SECURE)
+        # Create Minio storage if needed
+        try:
+            self.minio_client.make_bucket(settings.MINIO_BUCKET, location = "")
+        except BucketAlreadyOwnedByYou:
+            pass
+        except BucketAlreadyExists:
+            pass
+
+    def get_prefix(self):
+        return self.prefix
+
+    def clear_prefix(self, prefix = None):
+        if prefix is None:
+            prefix = self.prefix
+        try:
+            objects = self.minio_client.list_objects(settings.MINIO_BUCKET, prefix = self.prefix, recursive = True)
+            for obj in objects:
+                self.minio_client.remove_object(settings.MINIO_BUCKET, obj.object_name)
+        except ResponseError as err:
+            logging.info(err)
+
+    def put_file(self, local_path, remote_name):
+        self.minio_client.fput_object(settings.MINIO_BUCKET, remote_name, local_path)
+
+    def get_file(self, remote_name, local_path):
+        data = self.minio_client.get_object(settings.MINIO_BUCKET, remote_name)
+        with open(local_path, 'wb') as file_data:
+            for d in data.stream(32 * 1024):
+                file_data.write(d)
+
+    def download_and_put(self, url, remote_name):
+        """
+        Download a file and put it on Minio storage.
+        :param url: URL of the file to download
+        :param remote_name: file name in Minio storage
+        :return: the destination name if succeed, empty string otherwise
+        """
+        import urllib.request, tempfile
+        try:
+            f = urllib.request.urlopen(url)
+            with tempfile.NamedTemporaryFile(delete = True) as fp:
+                fp.write(f.read())
+                try:
+                    self.minio_client.fput_object(settings.MINIO_BUCKET, remote_name, fp.name)
+                except ResponseError as err:
+                    logging.info(err)
+                    return ''
+                return remote_name
+        except Exception as e:
+            logging.info(e)
+            return ''
