e7888d6810a175276999251d4be956f97645227e
Sanpi <sanpi@homecomputing.fr>
2018-02-20 13:22:01 +0100

    Use worker.sh for exodus service

--- exodus/worker.sh
+++ exodus/worker.sh
@@ -1,3 +1,3 @@
 source ../venv/bin/activate
-export DJANGO_SETTINGS_MODULE=exodus.settings.production
+export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-exodus.settings.production}
 python manage.py celery worker -A exodus.core -l info
