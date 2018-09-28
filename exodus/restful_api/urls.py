fc1004ad72f5c9a2e9cfbd3913858a94708da6ce
U+039b <u039b@exodus-privacy.eu.org>
2018-09-28 15:10:37 +0200

    Add compact reports details API endpoint

--- exodus/restful_api/urls.py
+++ exodus/restful_api/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     url(r'^apk/(?P<r_id>[0-9]+)/$', views.get_apk),
     url(r'^pcap/(?P<r_id>[0-9]+)/$', views.upload_pcap),
     url(r'^flow/(?P<r_id>[0-9]+)/$', views.upload_flow),
-    url(r'^reports$', views.get_all_reports),
+    url(r'^reports$', views.get_all_reports),  # deprecated
     url(r'^applications$', views.get_all_applications),
     url(r'^trackers$', views.get_all_trackers),
     url(r'^report/(?P<r_id>[0-9]+)/details$', views.get_report_details),
     url(r'^get_auth_token/$', rest_framework_views.obtain_auth_token, name='get_auth_token'),
+    url(r'^search/(?P<handle>.+)/details$', views.search_strict_handle_details, name = 'search_strict_handle_details'),
     url(r'^search/(?P<handle>.+)$', views.search_strict_handle, name = 'search_strict_handle'),
     url(r'^search$', views.search, name = 'search'),
 ]
