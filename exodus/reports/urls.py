25ad5dffb6f4b841983f26a75b75434099c58c08
simpnu <simon@pruneau.info>
2018-11-06 10:47:13 +0100

    adding new reports (no trackers, most trackers)
    Signed-off-by: simpnu <simon@pruneau.info>

--- exodus/reports/urls.py
+++ exodus/reports/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 app_name = 'reports'
 urlpatterns = [
-    url(r'^$', views.get_reports, name='index'),
+    url(r'^all/$', views.get_reports, name='index'),
+    url(r'^no_trackers/$', views.get_reports_no_trackers, name='get_reports_no_trackers'),
+    url(r'^most_trackers/$', views.get_reports_most_trackers, name='get_reports_most_trackers'),
     url(r'^(?P<report_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<app_id>[0-9]+)/icon$', views.get_app_icon, name='get_app_icon'),
     url(r'^apps/$', views.get_all_apps, name='get_all_apps'),
--- exodus/reports/views.py
+++ exodus/reports/views.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render
-from django.http import Http404
-from django.db import connection
-from exodus.core.dns import *
-from exodus.core.http import *
-from django.conf import settings
+
 import os
+
+from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db import connection
+from django.db.models import Count
+from django.http import Http404
+from django.http import HttpResponse
+from django.shortcuts import render
 from minio import Minio
+
+from exodus.core.dns import refresh_dns
+from reports.models import Report, Application
 
 
 def get_reports(request, handle=None):
@@ -22,7 +26,29 @@ def get_reports(request, handle=None):
         reports_paged = paginator.page(page)
     except Report.DoesNotExist:
         raise Http404("Reports do not exist")
-    return render(request, 'reports_list.html', {'reports': reports_paged, 'count': reports.count()})
+    return render(request, 'reports_list.html', {'reports': reports_paged, 'count': reports.count(), 'title': handle})
+
+
+def get_reports_no_trackers(request):
+    try:
+        reports = Report.objects.filter(found_trackers=None).order_by('-creation_date')
+        paginator = Paginator(reports, settings.EX_PAGINATOR_COUNT)
+        page = request.GET.get('page', 1)
+        reports_paged = paginator.page(page)
+    except Report.DoesNotExist:
+        raise Http404("Reports do not exist")
+    return render(request, 'reports_list.html', {'reports': reports_paged, 'count': reports.count(), 'title': 'no trackers'})
+
+
+def get_reports_most_trackers(request):
+    try:
+        reports = Report.objects.exclude(found_trackers=None).annotate(nb_trackers=Count('found_trackers')).order_by('-nb_trackers')
+        paginator = Paginator(reports, settings.EX_PAGINATOR_COUNT)
+        page = request.GET.get('page', 1)
+        reports_paged = paginator.page(page)
+    except Report.DoesNotExist:
+        raise Http404("Reports do not exist")
+    return render(request, 'reports_list.html', {'reports': reports_paged, 'count': reports.count(), 'title': 'most trackers'})
 
 
 def get_all_apps(request):
@@ -33,7 +59,6 @@ def get_all_apps(request):
     paginator = Paginator(apps_list, settings.EX_PAGINATOR_COUNT)
 
     page = request.GET.get('page')
-
 
     try:
         apps = paginator.page(page)
@@ -59,10 +84,12 @@ def refreshdns(request):
 
 
 def get_app_icon(request, app_id):
-    minioClient = Minio(settings.MINIO_URL,
-                access_key=settings.MINIO_ACCESS_KEY,
-                secret_key=settings.MINIO_SECRET_KEY,
-                secure=settings.MINIO_SECURE)
+    minioClient = Minio(
+        settings.MINIO_URL,
+        access_key=settings.MINIO_ACCESS_KEY,
+        secret_key=settings.MINIO_SECRET_KEY,
+        secure=settings.MINIO_SECURE
+    )
     try:
         app = Application.objects.get(pk=app_id)
     except Application.DoesNotExist:
