6d5dfadbd6e15cac6031728655f6a396490f84eb
simpnu <simon@pruneau.info>
2018-11-06 11:39:12 +0100

    replacing hardcoded urls by django references

--- exodus/search/urls.py
+++ exodus/search/urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import url
 
 from search.views import SearchView
 
+app_name = 'search'
 urlpatterns = [
-    url(
-        r'^$',
-        SearchView.as_view(),
-        name = 'search',
-    ),
+    url(r'^$', SearchView.as_view(), name='search'),
 ]
