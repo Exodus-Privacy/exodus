ed53dc3c7a2feae0af8d07045e008626c0723914
simpnu <simon@pruneau.info>
2018-11-25 18:25:33 +0100

    added fake user agent for google play check

--- exodus/analysis_query/models.py
+++ exodus/analysis_query/models.py
@@ -15,17 +15,18 @@ def validate_handle(value):
     if package_reg.match(value) is None:
         raise ValidationError(_(u'%s is not a valid application handle') % value)
 
-    r = requests.get('%s%s' % ('https://play.google.com/store/apps/details?id=', value))
+    fake_ua_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0'}
+    r = requests.get('%s%s' % ('https://play.google.com/store/apps/details?id=', value), headers=fake_ua_header)
     if r.status_code == 404:
         raise ValidationError(_(u'%s application not found on Google Play') % value)
 
     duplicate_queries = AnalysisRequest.objects.filter(processed = False, handle = value).count()
     if duplicate_queries > 0:
         raise ValidationError(_('This application is being analyzed.'))
+
     pending_queries = AnalysisRequest.objects.filter(processed = False).count()
     if pending_queries > 19:
         raise ValidationError(_('Too much pending requests, please retry later'))
-
 
 
 class AnalysisRequest(models.Model):
