27fcccb4a92e3d13402fec6c1cd332858bff6613
pnu <simon@pruneau.info>
2019-05-12 16:55:56 +0200

    Add search and filter fields for trackers in Admin page

--- exodus/trackers/admin.py
+++ exodus/trackers/admin.py
@@ -4,5 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Tracker, DetectionRule
 
-admin.site.register(Tracker)
-admin.site.register(DetectionRule)
+
+@admin.register(Tracker)
+class TrackerModelAdmin(admin.ModelAdmin):
+    date_hierarchy = 'creation_date'
+    search_fields = ['name']
+    list_display = ('name', 'code_signature', 'website')
+
+
+@admin.register(DetectionRule)
+class DetectionRuleModelAdmin(admin.ModelAdmin):
+    pass
