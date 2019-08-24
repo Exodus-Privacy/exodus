504873b4477ebc2e564bfe6c83d6f9e1fcad5900
Mélanie A. M <hello@ammelanie.fr>
2019-08-24 23:45:29 +0200

    test(api_applications): add

--- exodus/restful_api/tests.py
+++ exodus/restful_api/tests.py
@@ -3,4 +3,42 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIClient
+
+from reports.models import Application, Report
+
+
+class RestfulApiGetAllApplicationsTests(TestCase):
+
+    def test_returns_empty_json_when_no_applications(self):
+        client = APIClient()
+        response = client.get('/api/applications')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['applications'], [])
+
+    def test_returns_applications_with_report_last_update(self):
+        report = Report()
+        report.save()
+        application = Application(
+            name='app_name',
+            handle='handle',
+            report=report
+        )
+        application.save()
+
+        client = APIClient()
+        response = client.get('/api/applications')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, application.name, 1)
+
+        report_updated_at = application.report.updated_at
+        response_application = response.json()['applications'][0]
+
+        self.assertEqual(response_application['name'], application.name)
+        self.assertEqual(response_application['handle'], application.handle)
+        self.assertEqual(
+             response_application['report_updated_at'],
+             report_updated_at.timestamp()
+        )
