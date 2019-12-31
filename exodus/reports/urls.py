a784a12a31fed97617cb56e357ae0f71939e06d2
simpnu <simon@pruneau.info>
2019-12-31 11:16:26 +0100

    Use new Django URL scheme

--- exodus/reports/urls.py
+++ exodus/reports/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import RedirectView
 
 from . import views
 
 app_name = 'reports'
 urlpatterns = [
-    url(r'^$', views.get_reports, name='index'),
-    url(r'^(?P<report_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<app_id>[0-9]+)/icon$', views.get_app_icon, name='icon'),
-    url(r'^stats/$', RedirectView.as_view(pattern_name='trackers:get_stats', permanent=False)),
-    url(r'^search/(?P<handle>.+)/$', views.get_reports, name='search_by_handle'),
-    url(r'^(?P<handle>.+)/latest/$', views.detail, name='get_latest'),
-    url(r'^(?P<handle>.+)/latest/icon$', views.get_app_icon, name='icon_by_handle'),
+    path('', views.get_reports, name='index'),
+    path('<int:report_id>/', views.detail, name='detail'),
+    path('<int:app_id>/icon/', views.get_app_icon, name='icon'),
+    path('stats/', RedirectView.as_view(pattern_name='trackers:get_stats', permanent=False)),
+    path('search/<handle>/', views.get_reports, name='search_by_handle'),
+    path('<handle>/latest/', views.detail, name='get_latest'),
+    path('<handle>/latest/icon/', views.get_app_icon, name='icon_by_handle'),
 ]
