83ccd1fbd2444b0b088cd56f19772b82faf30517
U+039b <git@0x39b.fr>
2020-01-06 12:43:50 +0100

    Fix #302: * add MEN banner * change colors * override templates in separate Django app * use `INSTALLED_APPS = ['men_customization'] + INSTALLED_APPS` in production settings to apply customization * change favicon * change logo in menu * add new css file * create settings dedicated to customization
    Signed-off-by: U+039b <git@0x39b.fr>

---
+++ exodus/exodus/settings/men_customization.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+from __future__ import absolute_import
+from .base import *
+import os
+
+SECRET_KEY = '9b80473f1b0c7d9f1859cfa754e40e26'
+
+DEBUG = True
+ALLOWED_HOSTS = [u'localhost', u'127.0.0.1']
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'travisci',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'exodus',
+            'USER': 'exodus',
+            'PASSWORD': 'exodus',
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
+    }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '..', 'static'), )
+
+CELERY_BROKER_URL = 'amqp://guest@localhost//'
+BROKER_URL = CELERY_BROKER_URL
+MINIO_STORAGE_ENDPOINT = '127.0.0.1:9000'
+MINIO_STORAGE_ACCESS_KEY = 'AKIAIOSFODNN7EXAMPLE'
+MINIO_STORAGE_SECRET_KEY = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+
+ALLOW_APK_UPLOAD = True
+
+TRACKERS_AUTO_UPDATE = True
+
+# Customization
+INSTALLED_APPS = ['men_customization'] + INSTALLED_APPS
--- exodus/exodus/views.py
+++ exodus/exodus/views.py
@@ -5,28 +5,28 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html')
 
 
 def permissions(request):
-    return render(request, 'permissions.html')
+    return render(request, 'base/permissions.html')
 
 
 def trackers(request):
-    return render(request, 'trackers.html')
+    return render(request, 'base/trackers.html')
 
 
 def next(request):
-    return render(request, 'next.html')
+    return render(request, 'base/next.html')
 
 
 def understand(request):
-    return render(request, 'understand.html')
+    return render(request, 'base/understand.html')
 
 
 def organization(request):
-    return render(request, 'organization.html')
+    return render(request, 'base/organization.html')
 
 
 def page_not_found(request):
-    return render(request, '404.html')
+    return render(request, 'base/404.html')
