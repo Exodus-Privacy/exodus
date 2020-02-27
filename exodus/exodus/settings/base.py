27a3700f9e8dc60a75c8b766dd14dadd0378cce8
simpnu <simon@pruneau.info>
2020-02-27 20:49:53 +0100

    Remove XFrameOptionsMiddleware

--- exodus/exodus/settings/base.py
+++ exodus/exodus/settings/base.py
@@ -46,7 +46,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'exodus.urls'
