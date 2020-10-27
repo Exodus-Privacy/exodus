bf0c6d574e391061f5f0b36ab402152362a108eb
Pnu <6069449+pnu-s@users.noreply.github.com>
2020-10-27 13:21:53 +0100

    Adds possibility to overwrite CSRF_COOKIE_SECURE setting  (#395)
    * Refacto of dev & docker settings
    
    * Adds possibility to overwrite CSRF_COOKIE_SECURE setting

--- exodus/exodus/settings/dev.py
+++ exodus/exodus/settings/dev.py
@@ -1,12 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
-from .base import *
+from .common_dev import *
 import os
-
-SECRET_KEY = '9b80473f1b0c7d9f1859cfa754e40e26'
-
-DEBUG = True
-ALLOWED_HOSTS = ['*']
 
 if 'TRAVIS' in os.environ:
     DATABASES = {
@@ -31,16 +26,6 @@ else:
         }
     }
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '..', 'static'), )
-
 CELERY_BROKER_URL = 'amqp://guest@localhost//'
 BROKER_URL = CELERY_BROKER_URL
 MINIO_STORAGE_ENDPOINT = '127.0.0.1:9000'
-MINIO_STORAGE_ACCESS_KEY = 'exodusexodus'
-MINIO_STORAGE_SECRET_KEY = 'exodusexodus'
-
-ALLOW_APK_UPLOAD = True
-
-TRACKERS_AUTO_UPDATE = True
