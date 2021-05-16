c2e6dd17572501503e2c3b2564ab41298eb7fee3
Mickaël Wolff <mickael@lupusmic.org>
2021-05-16 08:00:44 +0200

    Use exec in docker entrypoint on commands that should replace current shell

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -72,11 +72,11 @@ function createUser() {
 }
 
 function startWorker() {
-    export C_FORCE_ROOT=1; celery worker --beat -A exodus.core -l info -S django
+    export C_FORCE_ROOT=1; exec celery worker --beat -A exodus.core -l info -S django
 }
 
 function startFrontend() {
-    python3 manage.py runserver 0.0.0.0:8000
+    exec python3 manage.py runserver 0.0.0.0:8000
 }
 
 function importTrackers() {
