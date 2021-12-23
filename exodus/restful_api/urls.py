d67846c8af5e4214fb2cbd6d57fbd02ebb7fa49a
pnu <simon@pruneau.info>
2021-12-23 14:17:26 +0100

    feat: New API endpoint to count elements (trackers, reports, apps)

--- exodus/restful_api/urls.py
+++ exodus/restful_api/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
     path('apk/<int:r_id>/', views.get_apk),
     path('reports', views.get_all_reports),  # deprecated
     path('applications', views.get_all_applications),
+    path('applications/count', views.get_applications_count, name='get_applications_count'),
     path('trackers', views.get_all_trackers),
+    path('trackers/count', views.get_trackers_count, name='get_trackers_count'),
     path('report/<int:r_id>/details', views.get_report_details),
+    path('reports/count', views.get_reports_count, name='get_reports_count'),
     path('get_auth_token/', rest_framework_views.obtain_auth_token, name='get_auth_token'),
     path('search/<handle>/details', views.search_strict_handle_details, name='search_strict_handle_details'),
     path('search/<handle>', views.search_strict_handle, name='search_strict_handle'),
