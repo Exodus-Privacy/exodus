74a05242d96d938977a0d6a03ada0c658313a6ef
Mickaël Wolff <mickael+github.com@lupusmic.org>
2022-01-18 08:55:19 +0100

    Improving docker tooling (#435)
    * Reorganize Dockerfile to have most changing files last
    
    * Rewrite docker entrypoint to have a usage message
    
    * Use exec in docker entrypoint on commands that should replace current shell
    
    * We don't need name for containers as we can use composer service names
    
    * Avoid user directory polution in bind-mounted source tree
    
    * Sort docker compose alphabetically
    
    * Python version might change one day
    
    * Detect there is no settings, give hint to create setting file
    
    * Launch the worker in its own container
    
    * Update docker related documentation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,56 +1,125 @@
 #!/bin/bash
 
+declare -r pyexe=python3
+declare -r pymanage="$pyexe manage.py"
+declare -r custom_docker_settings="/exodus/exodus/exodus/settings/custom_docker.py"
+
+declare -Ar commandList=(
+	[compile-messages]=compileMessages
+	[create-db]=createDb
+	[create-user]=createUser
+	[import-trackers]=importTrackers
+	[init]=init
+	[make-messages]=makeMessages
+	[refresh-fdroid-index]=refreshFdroidIndex
+	[start-frontend]=startFrontEnd
+	[start-worker]=startWorker
+)
+
+declare -Ar commandHelpList=(
+	[compile-messages]='compile translation messages'
+	[create-db]='create database if required and attend to migrations'
+	[create-user]='actually create a super user if not created yet'
+	[import-trackers]='import trackers from the official exodus instance'
+	[init]='Initialize database and launch Exodus'
+	[make-messages]='extract translation messages'
+	[refresh-fdroid-index]='fetch F-Droids list of packages'
+	[start-frontend]='lauch the local front end'
+	[start-worker]='start the exodus worker'
+)
+
+function main()
+{
+	getSettings
+
+	if [ -z "$1" ]
+	then
+		usage "$0"
+	elif [ -n "${commandList[$1]}" ]
+	then
+		"${commandList[$1]}"
+	else
+		exec "$@"
+	fi
+}
+
+function usage()
+{
+	cat <<- EOS
+		$1: prepare and lauch exodus
+
+	EOS
+
+	for cmd in "${!commandHelpList[@]}"
+	do
+		cat <<- EOS
+			$cmd: ${commandHelpList[$cmd]}
+		EOS
+	done
+}
+
 function getSettings() {
-    if [ -f "/exodus/exodus/exodus/settings/custom_docker.py" ]; then
+    if [ -f "$custom_docker_settings" ]; then
         export DJANGO_SETTINGS_MODULE=exodus.settings.custom_docker
     else
         export DJANGO_SETTINGS_MODULE=exodus.settings.docker
+		cat <<- EOS
+		File ${custom_docker_settings} doesn't exist, you might want to create it:
+
+			from .docker import *
+
+			DEBUG=True
+			GOOGLE_ACCOUNT_USERNAME = "<a valid google account>"
+			GOOGLE_ACCOUNT_PASSWORD = "<a valid google password>"
+			SECRET_KEY = "<a unique secret for Django>"
+		EOS
     fi
 }
 
 function createDB() {
-    echo 'CREATE EXTENSION IF NOT EXISTS pg_trgm;' | python3 manage.py dbshell
-    python3 manage.py makemigrations
-    python3 manage.py migrate
+	echo 'CREATE EXTENSION IF NOT EXISTS pg_trgm;' | $pymanage dbshell
+	$pymanage makemigrations
+	$pymanage migrate
 }
 
 function createUser() {
-    python3 manage.py createsuperuser
+	$pymanage createsuperuser
 }
 
 function startWorker() {
-    export C_FORCE_ROOT=1; celery worker --beat -A exodus.core -l info -S django
+	export C_FORCE_ROOT=1
+	exec celery worker --beat -A exodus.core -l info -S django
 }
 
 function startFrontend() {
-    python3 manage.py runserver 0.0.0.0:8000
+	exec $pymanage runserver 0.0.0.0:8000
 }
 
 function importTrackers() {
-    python3 manage.py import_categories
-    python3 manage.py importtrackers
+	$pymanage import_categories
+	$pymanage importtrackers
 }
 
 function makeMessages() {
-    python3 manage.py makemessages
+	$pymanage makemessages
 }
 
 function compileMessages() {
-    python3 manage.py compilemessages
+	$pymanage compilemessages
 }
 
 function refreshFdroidIndex() {
-    python3 manage.py refresh_fdroid_index
+	$pymanage refresh_fdroid_index
 }
 
 function init_db() {
-    while ! pg_isready -h db -p 5432 > /dev/null 2> /dev/null; do
-        echo "Connecting to db (postgresql) Failed: Waiting ..."
-        sleep 1
-    done
-    createDB
-    importTrackers
-    echo "Exodus DB is ready."
+	while ! pg_isready -h db -U exodus -p 5432 > /dev/null 2> /dev/null; do
+		echo "Connecting to db (postgresql) Failed: Waiting ..."
+		sleep 1
+	done
+	createDB
+	importTrackers
+	echo "Exodus DB is ready."
 }
 
 function init() {
@@ -58,34 +127,4 @@ function init() {
     startFrontend
 }
 
-getSettings
-
-case "${1}" in
-    "init")
-        init
-        ;;
-    "create-db")
-        createDB
-        ;;
-    "create-user")
-        createUser
-        ;;
-    "start-worker")
-        startWorker
-        ;;
-    "start-frontend")
-        startFrontend
-        ;;
-    "import-trackers")
-        importTrackers
-        ;;
-    "refresh-fdroid-index")
-        refreshFdroidIndex
-        ;;
-    "make-messages")
-        makeMessages
-        ;;
-    "compile-messages")
-        compileMessages
-        ;;
-esac
+main "$@"
