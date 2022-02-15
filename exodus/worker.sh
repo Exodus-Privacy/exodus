e7590d0ae703e34e1252cda972308c8cd9a77181
pnu <simon@pruneau.info>
2022-02-15 09:04:56 +0100

    fix: Update worker.sh to work with Celery 5

--- exodus/worker.sh
+++ exodus/worker.sh
@@ -1,3 +1,3 @@
 source ../venv/bin/activate
 export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-exodus.settings.production}
-celery worker -A exodus.core -l info
+celery -A exodus.core worker -l info
